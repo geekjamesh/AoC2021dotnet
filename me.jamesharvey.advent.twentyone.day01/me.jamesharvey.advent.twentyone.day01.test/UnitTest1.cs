a20c0930e36d038502c113481422bcf7e21028e4
geekjamesh <41005826+geekjamesh@users.noreply.github.com>
2021-12-01 18:30:01 +0000

    Initial Solution Set up

---
+++ me.jamesharvey.advent.twentyone.day01/me.jamesharvey.advent.twentyone.day01.test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace me.jamesharvey.advent.twentyone.day01.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ me.jamesharvey.advent.twentyone.day01/me.jamesharvey.advent.twentyone.day01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace me.jamesharvey.advent.twentyone.day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputValues = new List<string>();
+            using (var reader = new StreamReader("input.txt"))
+            {
+                string item = reader.ReadLine();
+                while(item != null) {
+                    inputValues.Add(item);
+                    item = reader.ReadLine();
+                }
+            }
+            try
+            {
+                foreach (string item in inputValues)
+                {
+                    Console.WriteLine(item);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error - {ex.Message}");
+            }
+            
+        }
+    }
+}
